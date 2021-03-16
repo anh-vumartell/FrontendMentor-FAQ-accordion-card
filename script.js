aa485ab2b0a3b2b6a536d5596bc1649557ba1d20
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2021-03-16 20:35:58 +0200

    modify script

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
 const accordionQuestion = document.getElementsByClassName(
-  "accordion__item--question"
+  "accordion--question"
 );
+
+
 let i;
 
 for (i = 0; i < accordionQuestion.length; i++) {
   accordionQuestion[i].addEventListener("click", function () {
     this.classList.toggle("active");
+    const accordionAnswer = this.nextElementSibling;
+    if(accordionAnswer.style.display === 'block'){
+      accordionAnswer.style.display = "none";
+      
+    } else{
+      accordionAnswer.style.display = "block";
+      
+    }
   });
 }
+
