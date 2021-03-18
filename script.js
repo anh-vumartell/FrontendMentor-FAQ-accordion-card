4b4d3de4d9c96c0a1624339f26be9a4dd04d2bb0
Ngoc Anh Vu-Martell <anh.vumartell@github.io>
2021-03-18 20:28:41 +0200

    run the function

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
-const accordionQuestions = document.querySelectorAll(".accordion--question");
+"use strict";
+const openFaq = function () {
+  const accordionQuestions = document.querySelectorAll(".accordion--question");
 
-const items = document.querySelectorAll(".accordion__item");
-accordionQuestions.forEach((question) => {
-  question.addEventListener("click", (e) => {
-    const currentItem = e.currentTarget.closest(".accordion__item");
-    items.forEach((item) => {
-      if (item !== currentItem) {
-        item.classList.remove("reveal-answer");
-      }
+  const items = document.querySelectorAll(".accordion__item");
+
+  accordionQuestions.forEach((question) => {
+    question.addEventListener("click", (e) => {
+      const currentItem = e.currentTarget.closest(".accordion__item");
+      items.forEach((item) => {
+        if (item !== currentItem) {
+          item.classList.remove("reveal-answer");
+        }
+      });
+      currentItem.classList.toggle("reveal-answer");
     });
-    currentItem.classList.toggle("reveal-answer");
   });
-});
+};
+
+openFaq();
